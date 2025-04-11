8e151eacfa213b27a6fbdf69a1ff5790041f5f5d
Andrei Bayla <147172993+A-R-E-I@users.noreply.github.com>
2025-04-11 10:20:22 -0400

    Add files via upload

--- accounts.py
+++ accounts.py
@@ -1,74 +1,52 @@
+from flask import Flask, render_template, request, redirect
 import os.path
 from os import path
 
-def account():
-    global filename, username, password
-    print("Enter Username and Password.\n");
-    username = str(input("Username: "));
-    password = str(input("Password: "));
-    filename = username + ".doc"
-    UserCheck()
-    
-def UserCheck():
-    if(username == "" or password == ""):
-        print("missing username or password");
-        account();
-    else:
-        FileCheck();
+global whichfilename;
+whichfilename = "LoginAccounts.doc";
 
-def FileCheck():
-    global adminfile, fileDir;
-    fileDir = os.path.dirname(os.path.realpath("__file__"));
-    fileexist = bool(path.exists(filename));
+app = Flask(__name__)
 
-    if(fileexist == False):
-        accountinfo = str(input("Account does not exists. Do you want to create this account? (1 for yes, 2 for no)"))
-        match(accountinfo):
-            case "1":
-                adminfile = open(filename,"x");
-                adminfile.close();
-                WriteToFile(username,password,filename);
-                filepath = fileDir + "\\Project-Supermarket-Menu.py";
-                filenamepath = {
-                    "__file__":filepath,
-                    "__name__":"__main__",
-                    };
-            case "2":
-                account();
-            case default:
-                FileCheck()
-
-        with open(filepath,"rb") as file:
-            exec(compile(file.read(), filepath, "exec"),filenamepath);
-    else:
-        adminfile = open(filename,"r");
-        checkpass(); 
-
-def checkpass():
-    adminvalue = adminfile.read().split(",")
-    adminfile.close();
-    userpwd = adminvalue[1].strip();
-    if(password != userpwd):
-        print("Wrong password. Try again");
-        account();
-    else:
-        filepath = fileDir + "\\Project-Supermarket-Menu.py";
-        filenamepath = {
-            "__file__":filepath,
-            "__name__":"__main__",
-            };
-    
-    with open(filepath,"rb") as file:
-        exec(compile(file.read(), filepath, "exec"),filenamepath);
-
-def WriteToFile(name,passwd,thefile):
-    adminfile = open(thefile,"w");
-    adminfile.write(name + "," + passwd);
-    adminfile.close();
-
+@app.route("/")
 def main():
-    account();
-  
+    return render_template("GetInformation.html")
+
+@app.route("/info",methods=["POST"])    
+def GetInfo():
+    global username
+    global userpasswd
+    username = request.form.get("txtusername")
+    userpasswd= request.form.get("txtpassword")
+    if(username == "" or userpasswd == ""):
+        return render_template("GetInformation.html");
+    else:
+        CreateCheckFile();
+        Infofilename = open(whichfilename,"r");
+        UserValue = Infofilename.read().split(",");
+        Infofilename.close();
+        username = UserValue[0].strip();
+        userpasswd= UserValue[1].strip();
+        return render_template("output3.html",username=username,password=userpasswd);
+
+def CreateCheckFile():
+    fileDir = os.path.dirname(os.path.realpath("__file__"));
+    fileexist = bool(path.exists(whichfilename));
+    if(fileexist == False):
+        status = "new";
+    else:
+        status = "edit"
+
+    WriteToFile(status);
+
+def WriteToFile(whichstatus):
+    if(whichstatus == "new"):
+        logacctfile = open(whichfilename,"x")
+        logacctfile.close();
+        logacctfile = open(whichfilename,"w")
+    else:
+        logacctfile = open(whichfilename,"a")
+
+    logacctfile.write(str(username) + "," + str(userpasswd));
+
 if __name__=="__main__":
-    main();
-      
+    app.run();
